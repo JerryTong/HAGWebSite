76c788ab4298142aa15ded635802edc0356c4dff
JerryTong <a866559@gmail.com>
2016-10-27 00:26:13 +0800

    create

---
+++ HAGWebSite/Content/Script/FrameWork.js
@@ -0,0 +1,13 @@
+﻿// header icon tooltip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+var UrlBuilder = {
+    ImagePath: "../Content/Image/",
+    ImageUrl: function (str) {
+        return this.ImagePath + str;
+    }
+}
+
+var imagePath = 'H:/HappyCoding/CShap/HAGWebSite/HAGWebSite/Content/Image';
---
+++ HAGWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HAGWebSite.Startup))]
+namespace HAGWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
